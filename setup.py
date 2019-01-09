bdfa805db5519ecf239d8aa9c8c5d70912effa93
Ivano Lauriola <ivanolauriola@gmail.com>
2019-01-09 23:12:01 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 from setuptools import setup, find_packages
 import os
 
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
   name = 'MKLpy',
@@ -26,48 +27,6 @@ setup(
                  'Programming Language :: Python :: 3.6',
                  'Topic :: Scientific/Engineering :: Artificial Intelligence',
                  'Topic :: Scientific/Engineering :: Mathematics',
-                 'License :: OSI Approved :: MIT License',
                 ],
-  long_description=
-'''
-
-MKLpy
-=====
-
-
-MKLpy is a framework for Multiple Kernel Learning and kernel machines scikit-compliant.
-
-This package contains:
-
-* MKL algorithms
-  * EasyMKL
-  * Average of kernels
-  * Soon available: GRAM, MEMO, SimpleMKL
-
-* tools to operate over kernels, such as normalization, centering, summation, mean...;
-
-* metrics, such as kernel_alignment, radius, margin, spectral ratio...;
-
-* kernel functions, such as homogeneous polynomial and boolean kernels (disjunctive, conjunctive, DNF, CNF).
-
-The 'examples' folder contains useful snippets of code.
-
-
-For more informations about classification, kernels and predictors visit `Link scikit-learn <http://scikit-learn.org/stable/>`_
-
-
-requirements
-------------
-
-To work properly, MKLpy requires:
-
-* numpy
-
-* scikit-learn
-
-* cvxopt
-
-
-
-''',
+  long_description = long_description,
 )
