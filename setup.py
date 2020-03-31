ff6324bcb0aa57e15d561e09a27df26bbbcc4413
ivanolauriola <ivanolauriola@gmail.com>
2020-03-31 19:31:44 +0200

    setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
   name = 'MKLpy',
   packages = find_packages(exclude=['build', '_docs', 'templates']),
-  version = '0.4.2b',
+  version = '0.4.3',
   install_requires=[
         "numpy",
         "scipy",
